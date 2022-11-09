966c3b5890dcc5dc7eea66e49ce00d72a9d1e28b
Keerthy Suresh <78009245+keerthysuresh@users.noreply.github.com>
2022-11-09 14:50:20 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the no:");
+scanf("%d"&a);
+if(a%2==0)
+printf("the number is even:");
+else
+printf("the number is odd");
+}
---
+++ postive.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the element");
+scanf("%d"&a);
+if (a<0)
+{
+printf(" the number is postive");
+}
+else
+{
+printf("the number is negative");
+
+}
+}
+
